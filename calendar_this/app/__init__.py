afc0158a44df16d3335f27acbc4d43edf3a446ac
lamarjd <jacob.lamar16@gmail.com>
2022-11-01 10:40:01 -0700

    App up and running

---
+++ calendar_this/app/__init__.py
@@ -0,0 +1,11 @@
+from app import routes
+from flask import Flask
+import os
+
+# creates the Flask app, gets the secret key from the environment, and registers the Blueprint. 
+
+
+# In the article, you were encouraged to create a Config class in a config module so that you could use app.config.from_object. This is an alternate way of doing it, the app.config.update method takes a dictionary and uses it in much the same way that app.config.from_object iterates over the attributes of the object and sets the configuration from that.
+app = Flask(__name__)
+app.config.update({'SECRET_KEY': os.environ.get('SECRET_KEY')})
+app.register_blueprint(routes.bp)
